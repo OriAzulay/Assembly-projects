0606ec41ec533c77f41a8678e86591af4dc8ee17
oriAZul <ori101914@gmail.com>
2021-12-02 13:48:31 +0200

    First step - is to write the main method: The main method is where the user will type the two strings and their size (Pstring1 & len1, Pstring2 &len2) Also, the user eill type the option he want (correct option will be 50,52,53,54,55 & 60) Then i wrote the switch-case scenario at func_select, where every caste has it's own method implemented by psting.s

---  func_select.s
+++  func_select.s
@@ -1,1 +1,47 @@
 # Ori Azulay, 206336794
+    .section    .rodata
+    .align 8                        # Align address to multiple of 8
+
+
+
+Jtable:                             # JUMP TABLE   
+    .quad   .L50                    # case 50 or 60           
+    .quad   .defualt                # case 51 goto defualt
+    .quad   .L52                    # case 52
+    .quad   .L53                    # case 53
+    .quad   .L54                    # case 54
+    .quad   .L55                    # case 55
+    .quad   .defualt                # case 51 goto defualt
+    .quad   .defualt                # case 51 goto defualt
+    .quad   .defualt                # case 51 goto defualt
+    .quad   .defualt                # case 51 goto defualt
+    .quad   .L50                    # case 60 return to case 50
+
+    .text                           # code start
+    .global run_func
+    .type   run_func, @function     # define 'run_func' function
+run_func:
+    leaq    -50(%rdi),%rdi          # start the stack from "0"
+    cmpq    $10,%rdi                # compare to 10
+    ja      .defualt                # if opt > 10 or negative (ja), goto defualt
+    jump    *.Jtable(,%rdi,8)       # for correct case, goto jump table
+    ret
+    
+    .type   defualt, @function
+.defualt:
+
+
+    .type   .L50, @function
+.L50:
+
+   .type   .L52, @function
+.L52:
+
+   .type   .L53, @function
+.L53:
+
+   .type   .L54, @function
+.L54:
+
+   .type   .L55, @function
+.L55:
--- run_main.s
+++ run_main.s
@@ -1,1 +1,48 @@
 # Ori Azulay, 206336794
+.section    .rodata
+.align 8                        # Align address to multiple of 8
+# values for scanf
+in_len1:    .string "%d"        # len1
+in_string1: .string "%s"        # Pstring1
+in_len2:    .string "%d"        # len2
+in_string2: .string "%s"        # Pstring2
+in_opt:     .string "%d"        # opt
+
+    .text
+    .global run_main
+    .type   run_main, @function # define 'run_main' function
+run_main:
+    pushq   %rbp                # start the stack-frame by the callee-saver
+    movq    %rsp,%rbp           # call rsp as pointer to the end of stack
+    subq    $512,%rsp           # make room for 2 Pstrings and two ('\0')
+
+    movq    $in_len1,%rdi       # load register for Pstring1 size
+    leaq    -256(%rbp),%rsi     # set address for 1st size, len1, at end of 1st string
+    movq    $0,%rax             # clear rax before scanf   
+    call    scanf
+
+    movq    $in_string1,%rdi    # load register for Pstring1 
+    leaq    -255(%rbp),%rsi     # set address for 1st string, string goes from bottum->top
+    movq    $0,%rax             # clear rax before scanf   
+    call    scanf
+
+    movq    $in_len2,%rdi       # load register for Pstring1 size
+    leaq    -512(%rbp),%rsi     # set address for 2nd size, len1, at the end of 2nd string
+    movq    $0,%rax             # clear rax before scanf   
+    call    scanf
+
+    movq    $in_string2,%rdi    # load register for Pstring2, string goes from bottum->top
+    leaq    -511(%rbp),%rsi     # set address for 2md Pstring
+    movq    $0,%rax             # clear rax before scanf   
+    call    scanf
+
+    movq    $in_opt,%rdi         # load register for the user option
+    subq    $16,%rsp            # make more room for the option    
+    leaq    (%rsp),%rsi         # configure where is the option scanned          
+    addq    $16,%rsp            # deallocate space
+    call    run_func            # call the function
+
+    addq    $512,%rsp           # end of the main function
+    movq    $0,%rax             # clear rax
+    popq    %rbp                # end the stack-frame 
+    ret
