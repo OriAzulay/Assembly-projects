e2906dd4e1c48cc98a19837804934f6e9f78b162
oriAZul <ori101914@gmail.com>
2021-12-04 21:33:21 +0200

    Adding case 52 - replace oldChar by newChar

--- func_select.s
+++ func_select.s
@@ -3,8 +3,13 @@
     .align 8                        # Align address to multiple of 8
 
 # string to printf
-len_50:         .string "first pstring length: %d, second pstring length: %d\n"
+case_50:        .string "first pstring length: %d, second pstring length: %d\n"
+case_52:        .string "old char: %c, new char: %c, first string: %s, second string: %s\n"
 def_option:     .string "invalid option!\n"
+
+# values for scanf
+char_in:        .string " %c %c"
+uchar_in:       .string " %hhu"     # a char for number-input
 
 jump_table:                         # JUMP TABLE   
     .quad   .L50                    # case 50 or 60           
@@ -13,22 +18,23 @@ jump_table:                         # JUMP TABLE
     .quad   .L53                    # case 53
     .quad   .L54                    # case 54
     .quad   .L55                    # case 55
-    .quad   .default                # case 51 not exist, goto defualt
-    .quad   .default                # case 51 not exist, goto defualt
-    .quad   .default                # case 51 not exist, goto defualt
-    .quad   .default                # case 51 not exist, goto defualt
+    .quad   .default                # case 56 not exist, goto defualt
+    .quad   .default                # case 57 not exist, goto defualt
+    .quad   .default                # case 58 not exist, goto defualt
+    .quad   .default                # case 59 not exist, goto defualt
     .quad   .L50                    # case 60 return to case 50
 
     .text                           # code start
     .global run_func
     .type   run_func, @function     # define 'run_func' function
 run_func:
-    subq    $50,%rdi                # start the switch-case from "0"
-    cmpq    $10,%rdi                # compare to 10 
+    subq    $50,%rdi                # compute opti = opt-50
+    cmpq    $10,%rdi                # compare opii to 10 
     ja      .default                # if 'opt' > 10 or negative (ja), goto defualt
-    jmp     *jump_table(,%rdi,8)    # for correct case, goto jump table
+    jmp     *jump_table(,%rdi,8)    # for correct case, goto jump table[opti]
     ret
     
+# case defualt - if opt <50 , opt > 60 or opt==51,56,57,58,59
     .type   default, @function
 .default:
     movq    $def_option, %rdi       # load format of printf %d for 'invalid'
@@ -36,6 +42,7 @@ run_func:
     call printf                     # call message
     ret
 
+# case 50 or 60 - function to return strings length
     .type   L50, @function
 .L50:
     movq    %rsi,%rdi               # move 1st pstrlen's address to rdi (by convention)
@@ -47,13 +54,42 @@ run_func:
     call    pstrlen                 # call func to get 2nd string size
     popq    %rsi                    # pop 1st string's size to rsi for printing
     movq    %rax,%rdx               # move the second string size for printing
-    movq    $len_50,%rdi            # load format string for printing
+    movq    $case_50,%rdi           # load format string for printing
     movq    $0,%rax                 # clear rax before printf
     call printf                     # print
     ret
     
+# case 52 - function to replace oldChar by newChar
     .type   L52, @function
 .L52:
+    pushq   %rsi                    # push address of pstring1
+    pushq    %rdx                    # push address of pstring2
+    subq    $8,%rsp                 # load space for user's input
+    movq    $char_in,%rdi           # format %c for user input
+    leaq    (%rsp),%rsi             # register rsi for oldChar
+    leaq    1(%rsp),%rdx            # register rdi for newChar
+    movq    $0,%rax                 # clear rax before scanf
+    call    scanf
+    movzbq  (%rsp),%rsi             # move first byte of oldChar to register
+    movzbq  1(%rsp),%rdx            # move first byte of newChar to register
+    movq    16(%rsp),%rdi           # move pstring1's address to rdi before function
+    movq    $0,%rax                 # clear rax before calling a function
+    call    replaceChar             # for pstring-1
+    movzbq  (%rsp),%rsi             # move first byte of oldChar to register
+    movzbq  1(%rsp),%rdx            # move first byte of newChar to register
+    movq    8(%rsp),%rdi            # move pstring2's address to rdi before function
+    movq    $0,%rax                 # clear rax before calling a function
+    call    replaceChar             # for pstring-2
+    movq    $case_52,%rdi            # format for printing message
+    movq    (%rsp),%rsi             # first char to rsi
+    movq    1(%rsp),%rdx            # second char to rdi
+    addq    $8,%rsp                 # deallocate char's space
+    popq    %r8                     # pop pstring2 to r8
+    leaq    1(%r8),%r8              # set address of r8 to start of pstring2
+    popq    %rcx                    # pop pstring1 to rcx   
+    leaq    1(%rcx),%rcx            # set address of rcx to start of pstring1
+    movq    $0,%rax                 # clear rax before printf
+    call    printf
     ret
     
     .type   L53, @function
--- pstring.s
+++ pstring.s
@@ -10,3 +10,21 @@
 pstrlen:
     movb    (%rdi),%al          # go to the first byte of the string, there's the size
     ret
+
+# replaceChar - function that gets from the user old char to replace
+# and new char to replace, and replace it on both strings
+    .global replaceChar
+    .type   replaceChar, @function
+replaceChar:
+    movq    $0,%r10             # define r10 register as counter
+    for_rep:
+    cmpb    (%rdi),%r10b        # check if we ran all over the string (if count==len)
+    je      end_rep             # go to end
+    inc     %r10                # counter ++
+    cmpb    %sil,(%rdi,%r10,1)  # check if pstring[counter] == newChar
+    jne     for_rep             # else, go to next itration
+    mov     %dl,(%rdi,%r10,1)   # replace oldChar by the newChar
+    jmp     for_rep
+    end_rep:
+    movq    %rdi,%rax           # return address of the string bt convention
+    ret
